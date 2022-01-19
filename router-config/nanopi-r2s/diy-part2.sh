b2e8ee6a49c32744722c867b5c1aad2518820daa
markbm <markbm@outlook.com>
2022-01-19 22:44:45 +0800

    Update diy-part2.sh

--- router-config/nanopi-r2s/diy-part2.sh
+++ router-config/nanopi-r2s/diy-part2.sh
@@ -15,6 +15,11 @@
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
+echo -e '\nOpenwrt by M.A.R.C@'$(date "+%Y.%m.%d")'\n'  >> package/base-files/files/etc/banner
+sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION='$(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='Openwrt by M.A.R.C@$(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
 
