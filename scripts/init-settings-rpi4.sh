9556ccfd5a212ffd489de8a18bc4cf4a646aa0f1
markbm <markbm@outlook.com>
2022-01-20 20:31:05 +0800

    corrected errors in script

--- scripts/init-settings-rpi4.sh
+++ scripts/init-settings-rpi4.sh
@@ -3,13 +3,14 @@
 # File name: init-settings-rpi4.sh
 # Description: This script will be executed during the first boot
 #=================================================
-uci set luci.dhcp.wan=dhcp
-uci set luci.dhcp.wan.interface='wan'
-uci set luci.dhcp.wan.ignore='1'
-uci set luci.network.wan=interface
-uci set luci.network.wan.device='eth0'
-uci set luci.network.wan.proto='dhcp'
-uci set luci.firewall.@zone[1].network='wan'
+
+uci set dhcp.wan=dhcp
+uci set dhcp.wan.interface='wan'
+uci set dhcp.wan.ignore='1'
+uci set network.wan=interface
+uci set network.wan.device='eth0'
+uci set network.wan.proto='dhcp'
+uci set firewall.@zone[1].network='wan'
 uci commit
 
 exit 0
