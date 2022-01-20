225b70e2154b0c42e6634bf8d391fa3778f9bba6
markbm <markbm@outlook.com>
2022-01-20 20:23:22 +0800

    Create init-settings-rpi4.sh

---
+++ init-settings-rpi4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+# File name: init-settings.sh
+# Description: This script will be executed during the first boot
+#=================================================
+uci set luci.dhcp.wan=dhcp
+uci set luci.dhcp.wan.interface='wan'
+uci set luci.dhcp.wan.ignore='1'
+uci set luci.network.wan=interface
+uci set luci.network.wan.device='eth0'
+uci set luci.network.wan.proto='dhcp'
+uci set luci.firewall.@zone[1].network='wan'
+uci commit
+
+exit 0
