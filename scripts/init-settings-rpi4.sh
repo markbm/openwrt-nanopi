9d2abf0d042b3242ce1f888a94734c072cb43c7c
markbm <markbm@outlook.com>
2022-01-20 23:04:29 +0800

    Update init-settings-rpi4.sh

--- scripts/init-settings-rpi4.sh
+++ scripts/init-settings-rpi4.sh
@@ -8,7 +8,7 @@ uci set dhcp.wan=dhcp
 uci set dhcp.wan.interface='wan'
 uci set dhcp.wan.ignore='1'
 uci set network.wan=interface
-uci set network.wan.device='eth0'
+uci set network.wan.device='eth1'
 uci set network.wan.proto='dhcp'
 uci set firewall.@zone[1].network='wan'
 uci commit
