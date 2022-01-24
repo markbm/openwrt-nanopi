a3c176c480c201f7bca6e0e9fc9ce7e274aee0c0
markbm <markbm@outlook.com>
2022-01-24 10:21:38 +0800

    Update init-settings-nanopi_r2s.sh

--- scripts/init-settings-nanopi_r2s.sh
+++ scripts/init-settings-nanopi_r2s.sh
@@ -4,10 +4,10 @@
 # Description: This script will be executed during the first boot
 #=================================================
 
-network.usb0=interface
-network.usb0.proto='dhcp'
-network.usb0.device='usb0'
-network.usb0.auto='0'
+uci set network.usb0=interface
+uci set network.usb0.proto='dhcp'
+uci set network.usb0.device='usb0'
+uci set network.usb0.auto='0'
 uci commit
 
 exit 0
