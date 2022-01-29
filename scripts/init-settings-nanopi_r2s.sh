1ce9c7b365242e9af61642fd0fe5ee3f935c3686
markbm <markbm@outlook.com>
2022-01-29 09:40:05 +0800

    Update init-settings-nanopi_r2s.sh

--- scripts/init-settings-nanopi_r2s.sh
+++ scripts/init-settings-nanopi_r2s.sh
@@ -9,6 +9,10 @@ uci set network.usb0.proto='dhcp'
 uci set network.usb0.device='usb0'
 uci set network.usb0.auto='0'
 uci set firewall.@zone[1].network='wan usb0'
+uci set system.@system[0].hostname='NanoPi-R2S'
+uci set system.@system[0].zonename='Asia/Kuala Lumpur'
+uci set system.@system[0].timezone='<+08>-8'
+
 uci commit
 
 sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
