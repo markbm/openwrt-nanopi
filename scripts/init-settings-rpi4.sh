3aed34aabb58b0367f1063cf328a8ab7bfa0435f
markbm <markbm@outlook.com>
2022-02-04 14:31:05 +0800

    Update init-settings-rpi4.sh

--- scripts/init-settings-rpi4.sh
+++ scripts/init-settings-rpi4.sh
@@ -15,7 +15,7 @@ uci set network.usb0.proto='dhcp'
 uci set network.usb0.device='usb0'
 uci set network.usb0.auto='0'
 uci set firewall.@zone[1].network='wan usb0'
-uci set system.@system[0].hostname='NanoPi-R2S'
+uci set system.@system[0].hostname='RaspberryPi4'
 uci set system.@system[0].zonename='Asia/Kuala Lumpur'
 uci set system.@system[0].timezone='<+08>-8'
 uci commit
