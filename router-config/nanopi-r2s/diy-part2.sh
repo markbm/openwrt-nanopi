d5a2e8d50df2b2e2752dd1da757cc7b4022e26c3
markbm <markbm@outlook.com>
2022-09-21 17:15:40 +0800

    Update diy-part2.sh

--- router-config/nanopi-r2s/diy-part2.sh
+++ router-config/nanopi-r2s/diy-part2.sh
@@ -15,13 +15,13 @@
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
-echo -e '\nOpenwrt by M.A.R.C\n'  >> package/base-files/files/etc/banner
-sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION='$(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
-sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='Openwrt by M.A.R.C'" >> package/base-files/files/etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
+# echo -e '\nOpenwrt by M.A.R.C\n'  >> package/base-files/files/etc/banner
+# sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+# echo "DISTRIB_REVISION='$(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
+# sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+# echo "DISTRIB_DESCRIPTION='Openwrt by M.A.R.C'" >> package/base-files/files/etc/openwrt_release
+# sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+# echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
